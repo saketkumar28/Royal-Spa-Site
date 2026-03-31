1a0322d3524d82b732024c7e12a3d9fbc9d3ee85
saketkumar28 <kumarsaket2801@gmail.com>
2026-03-31 23:31:35 +0530

    New Changes

--- Backend/Services/email.service.js
+++ Backend/Services/email.service.js
@@ -10,6 +10,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false, // <-- THIS IS THE MAGIC LINE FOR RENDER
+  },
 });
 
 const brandHeader = `
